85ea1fbe3b9a95a242dbbfc527c35cea3155300f
jastigi <jastigi@gmail.com>
2026-01-13 13:31:09 +0100

    Initial Commit

---
+++ src/main/java/com/jastigi/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.jastigi.curso.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/app")
+    public String index() {
+        return "Ok 200";
+    }
+
+}
